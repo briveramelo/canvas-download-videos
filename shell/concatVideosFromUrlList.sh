ed9efb59b4fb63c67ae908f0aff728b8a9cff3fa
Brandon Rivera-Melo <brandon.rivera-melo@hsc.utah.edu>
2020-10-10 12:51:01 -0600

    comments added for use clarity, indentation fixed for the for loop

--- shell/concatVideosFromUrlList.sh
+++ shell/concatVideosFromUrlList.sh
@@ -1,7 +1,8 @@
 #!/usr/local/bin/bash
 
 #this script takes a file with a list of video urls and concatenates them into a single, compressed output .mp4 video
-#run this script like "./concatVideosFromUrlList.sh ./myVideoUrlsFile.txt ./myConcatenatedVideoOutput.mp4"
+#run this script like "./concatVideosFromUrlList.sh ./myVideoUrlsDirectory" and all .txt files within will be made into a .mp4
+#or run this script like "./concatVideosFromUrlList.sh ./myVideoUrlsFile.txt ./myConcatenatedVideoOutput.mp4" for a single file
 #where myVideoUrlsFile.txt is a newline-separated list of urls, starting with file
 #eg:
 #file https://www.example.com/myVideoUrl1
@@ -9,14 +10,14 @@
 #...
 VIDEO_URLS_INPUT_PATH=$1
 if [[ -d $VIDEO_URLS_INPUT_PATH ]]; then
-  echo "creating videos for all .txt files in this folder"
+  echo "creating videos for all .txt files in this folder at the time the script was started"
   files=($( ls "$VIDEO_URLS_INPUT_PATH" ))
   for ((i=0; i<${#files[@]}; i++)); do
     #do something to each element of array
     files[$i]="$VIDEO_URLS_INPUT_PATH/${files[$i]}"
     echo "${files[$i]%.*}"
     ffmpeg -f concat -safe 0 -protocol_whitelist "file,http,https,tcp,tls" -i "${files[$i]}" -vcodec libx264 -crf 28 "${files[$i]%.*}.mp4"
-done
+  done
 else
   OUTPUT_PATH=$2
   ffmpeg -f concat -safe 0 -protocol_whitelist "file,http,https,tcp,tls" -i "$VIDEO_URLS_INPUT_PATH" -vcodec libx264 -crf 28 "$OUTPUT_PATH"
